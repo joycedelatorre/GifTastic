accba0aafd2aa1ae1ca17f5cb120451180a2996b
joyce <joycedelatorre87@gmail.com>
2017-11-21 16:03:49 -0600

    add feature of ratings

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,52 +1,54 @@
 $(document).ready(function(){
   //console.log("ready");
-
-  $("#submit").on("click", function(event){
+//create a button that has the same name of the user input at the form whenever we hit 
+//the submit button
+  $("#submit").on("click", function(event){  
     event.preventDefault();
     var addButton = $("<button>");
     var input_entry = $("#input").val();
-    console.log(input_entry);
+    //console.log(input_entry);
     addButton.text(input_entry);
     addButton.attr("data-character", input_entry);
     $(".btn-List").append(addButton);
     addEvent();
+// line 13 calls the event function so that the button created will be able to call the api.
   })
 
   function addEvent(){
     $("button").on("click",function(){
       var character = $(this).attr("data-character");
-      var queryURL ="https://api.giphy.com/v1/gifs/search?q="+character+"&api_key=SfCF9Z6EujiYx3XUsOSL5693ZqIirCsK&limit=5";
-
+      var queryURL ="https://api.giphy.com/v1/gifs/search?q="+character+"&api_key=SfCF9Z6EujiYx3XUsOSL5693ZqIirCsK&limit=10";
       $.ajax({
         url: queryURL,
         method:"GET"
       }).done(function(response){
         var results = response.data;
-        for(var i=0; i<results.length; i++){
+        for(var i=0; i<results.length; i++){ 
+//--------we loop into the response.data to create a div and place an image
+          var rating = results[i].rating;
+          //console.log(rating);
+          var p = $("<p>").text("Rating: " + rating); // to get the ratings of each image 
           var characterDiv = $("<div>");
           var characterImage = $("<img>");
-          characterImage.attr("src", results[i].images.fixed_height.url);
-          //console.log(results[i].images);
+//--------console.log(results[i].images);
+          characterImage.attr("src", results[i].images.original_still.url);
           characterImage.attr("still", results[i].images.original_still.url);
           characterImage.attr("animated",results[i].images.fixed_height.url);
           characterDiv.append(characterImage); 
+          characterDiv.append(p);
           $("#gifList").prepend(characterDiv);
-          // $(characterImage).on("click", function(event){
-          //   $(this).attr("src", $(this).attr("still"));
-          //   $(this).unbind(event);
-          // });
-          
-          $(characterImage).mouseover(
-            function()
-            {
-              $(this).attr("src", $(this).attr("still")); 
-            });
-          $(characterImage).mouseout(
-            function()
-            {
-              // $(this).attr("src", $(this).attr("animated"));
-              $(this).attr("src", $(this).attr("animated")); 
+//-------
+          $(characterImage).on("click", function(){
+            var currentVal= $(this).attr("src");
+            var animatedVal = $(this).attr("animated");
+            var stillVal =$(this).attr("still");
+            if(currentVal === stillVal){
+              $(this).attr("src", animatedVal); 
+            } else{
+              $(this).attr("src", stillVal);
+            }
           });
+          
         }
       }); //end of ajax
     });
